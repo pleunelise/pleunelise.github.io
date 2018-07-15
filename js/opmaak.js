b57a1c7c92fab853d020e21473431d73f2a197f8
Pleun Emmelot <pleunelise.emmelot@coderclass.nl>
2018-07-15 16:37:44 +0200

    add js file

---
+++ js/opmaak.js
@@ -0,0 +1,6 @@
+function functionTour() {
+  document.getElementById("tour").innerHTML = "Hello World";
+}
+function functionZelf() {
+  document.getElementById("zelf").innerHTML = "Hello World";
+}
