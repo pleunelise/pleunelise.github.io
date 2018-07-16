09de846472bbd92690f9a9ae6d89514b76a589e2
Pleun Emmelot <pleunelise.emmelot@coderclass.nl>
2018-07-16 09:42:39 +0200

    added function myFunction and changeColor

--- opmaak.js
+++ opmaak.js
@@ -8,7 +8,26 @@ function normalImg(x) {
     x.style.width = "25px";
 }
 
- var kleur = document.getElementById("colorFooter");
+var kleur1 = "#A0C6F0";
+var kleur2 = "#D4E6FB";
+function myFunction() {
+  var footer = document.getElementById("footer");
+  var body = document.getElementById("body");
+  var h1 = document.getElementById("h1");
+  footer.style.background = kleur2;
+  body.style.background = kleur1;
+  h1.style.color = kleur2;
+}
+
+function changeColor() {
+  var footer = document.getElementById("footer");
+  var body = document.getElementById("body");
+  var h1 = document.getElementById("h1");
+  footer.style.background = kleur1;
+  body.style.background = kleur2;
+  h1.style.color = kleur1;
+}
+/* var kleur = document.getElementById("colorFooter");
 kleur.addEventListener("click", veranderKleur);
 
 function veranderKleur() {
@@ -18,3 +37,4 @@ function veranderKleur() {
       document.getElementById("colorFooter").style.background = kleuren;
     }
 }
+*/
